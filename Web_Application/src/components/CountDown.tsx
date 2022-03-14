46dee36b9c09ec0721cb6a43bd1d0781e0e11711
KartikWatts <arorakartik4@gmail.com>
2022-03-14 20:48:46 +0530

    countdown timer music for web app

--- Web_Application/src/components/CountDown.tsx
+++ Web_Application/src/components/CountDown.tsx
@@ -1,12 +1,18 @@
 import React, { useEffect, useState } from "react";
+import countdown_timer from "../assets/sounds/countdown_time.mp3";
+import useSound from "use-sound";
 
 function CountDown() {
 	const [countDown, setCountDown] = useState("120");
 	const [countDownTime, setCountDownTime] = useState(0);
+	const [isFinalSeconds, setIsFinalSeconds] = useState(false);
+
+	const [playCountDownTimer, { stop: stopCountDownTimer }] =
+		useSound(countdown_timer);
 
 	useEffect(() => {
 		// TODO:: TO BE REPLACED BY TIME OF THE GAME ON THE SERVER IF EXISTS
-		let cTime = new Date(+new Date() + 60000 * 2).getTime(); //Adding two minutes to current time
+		let cTime = new Date(+new Date() + 60000 * 0.2).getTime(); //Adding two minutes to current time
 		if (!countDownTime) {
 			setCountDownTime(cTime);
 			return;
@@ -24,12 +30,34 @@ function CountDown() {
 			);
 			let secondsStr = seconds.toString();
 			setCountDown(secondsStr);
-
-			if (seconds == 0) clearInterval(countDownInterval);
+			if (seconds <= 10) {
+				setIsFinalSeconds(true);
+			}
+			if (seconds == 0) {
+				clearInterval(countDownInterval);
+			}
 		}, 1000);
 	}, [countDownTime]);
 
-	return <div className="text-slate-300 text-2xl">{countDown}</div>;
+	useEffect(() => {
+		if (countDown == "10") {
+			playCountDownTimer();
+		}
+		if (countDown == "0") {
+			stopCountDownTimer();
+		}
+	}, [countDown]);
+
+	return (
+		<div
+			id="countdown_timer"
+			className={`${
+				isFinalSeconds ? "text-red-400" : "text-slate-300"
+			}   text-2xl`}
+		>
+			{countDown}
+		</div>
+	);
 }
 
 export default CountDown;
