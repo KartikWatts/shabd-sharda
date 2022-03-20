8b5c0074e0ac4cda8f5fb913fea38d6d547d8b54
KartikWatts <arorakartik4@gmail.com>
2022-03-20 06:39:56 +0530

    implemented google, github, facebook oAuth functionality

--- Web_Application/src/App.tsx
+++ Web_Application/src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { json } from "stream/consumers";
+import UserAuth from "./components/UserAuth";
 import Game from "./containers/Game";
 import ResultDisplay from "./containers/ResultDisplay";
 import { GameContext } from "./contexts/GameContext";
@@ -8,30 +9,33 @@ function App() {
 	const gameContext = useContext(GameContext);
 
 	return (
-		<div className="w-screen h-screen bg-slate-600 flex justify-center items-center flex-col">
-			{gameContext && gameContext.isGameOn ? (
-				<Game />
-			) : gameContext && gameContext.wordsList.length > 0 ? (
-				<ResultDisplay
-					gameArray={gameContext.gameArray}
-					wordsList={gameContext.wordsList}
-					score={gameContext.score}
-				/>
-			) : (
-				<div>
-					<button
-						className="bg-yellow-300 p-8 border-4 hover:bg-amber-300 border-blue-400 rounded-lg text-blue-600 transition-all text-2xl drop-shadow-xl hover:drop-shadow-2xl font-bold hover:scale-105"
-						onClick={() => {
-							if (gameContext) {
-								gameContext.toggleGameState();
-							}
-						}}
-					>
-						Start Game
-					</button>
-				</div>
-			)}
-		</div>
+		<>
+			<UserAuth />
+			<div className="w-screen h-screen bg-slate-600 flex justify-center items-center flex-col">
+				{gameContext && gameContext.isGameOn ? (
+					<Game />
+				) : gameContext && gameContext.wordsList.length > 0 ? (
+					<ResultDisplay
+						gameArray={gameContext.gameArray}
+						wordsList={gameContext.wordsList}
+						score={gameContext.score}
+					/>
+				) : (
+					<div>
+						<button
+							className="bg-yellow-300 p-8 border-4 hover:bg-amber-300 border-blue-400 rounded-lg text-blue-600 transition-all text-2xl drop-shadow-xl hover:drop-shadow-2xl font-bold hover:scale-105"
+							onClick={() => {
+								if (gameContext) {
+									gameContext.toggleGameState();
+								}
+							}}
+						>
+							Start Game
+						</button>
+					</div>
+				)}
+			</div>
+		</>
 	);
 }
 
